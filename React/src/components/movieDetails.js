a4c3f32ee6036233e84820c59f8391c811b5009a
Nikita <91719405+landarenko0@users.noreply.github.com>
2023-12-13 18:30:41 +0500

    Added comments, improved readability

--- React/src/components/movieDetails.js
+++ React/src/components/movieDetails.js
@@ -1,44 +1,64 @@
 import React from "react";
 
+// Фрагмент деталей фильма
 function MovieDetails(props) {
-    const allActors = props.movie.actors.split(", ")
-    const actors = allActors.map((actor, index) => <div className="actor" key={index}><p>{actor}</p></div>)
+    const splittedActors = props.movie.actors.split(", ")
+    const actors = splittedActors.map((actor, index) => <div className="actor" key={ index }><p>{ actor }</p></div>)
+
     return (
         <div className="movieDetails">
+
+            { /* Верхняя часть фрагмента (ID и блок "Редактировать") */ }
             <div className="movieDetailsHeader">
-                <label>ID: <label id="id">{ props.movie.id }</label> <img className="copyButton" src="copy.svg" alt="" onClick={ () => { navigator.clipboard.writeText(document.querySelector("#id").innerText) } }></img></label>
+
+                { /* По нажатию на значок будет скопирован ID фильма */ }
+                <label>ID: <label id="id">{ props.movie.id }</label> <img className="copyButton" src="copy.svg" alt="" onClick={ () => { navigator.clipboard.writeText(document.querySelector("#id").innerText) } } /></label>
+                
                 <div className="edit" onClick={ () => { props.onEditClick() } }>
-                    <label className="editText"><img className="editText" src="edit.svg" alt=""></img> Редактировать</label>
+                    <label className="editText"><img src="edit.svg" alt=""></img> Редактировать</label>
                 </div>
             </div>
+
+            { /* Обертка деталей фильма */ }
             <div className="movieDetailsWrapper">
-                <img className="poster" alt="" src={props.movie.posterUrl}></img>
+                <img className="poster" alt="" src={ props.movie.posterUrl } />
+
                 <div>
-                    <p><b className="titleStyle">{props.movie.title}</b></p>
-                    <p className="directorStyle">{props.movie.director}</p>
+                    <p><b className="titleStyle">{ props.movie.title }</b></p>
+                    <p className="directorStyle">{ props.movie.director }</p>
+
                     <div className="detailsWrapper">
+
+                        { /* Детали фильма */ }
                         <div className="parameters">
                             <b>Параметры</b>
+
                             <div className="parameter">
                                 <label className="parameterNameStyle">Год производства</label>
                                 <label>{ props.movie.year }</label>
                             </div>
+
                             <div className="parameter">
                                 <label className="parameterNameStyle">Длительность (мин)</label>
                                 <label>{ props.movie.runtime }</label>
                             </div>
+
                             <div className="parameter">
                                 <label className="parameterNameStyle">Жанры</label>
                                 <label>{ props.movie.genres.join(", ") }</label>
                             </div>
                         </div>
+
+                        { /* Актеры */ }
                         <div>
-                            <b>В главных ролях <img src="checkMark.svg" alt=""></img></b>
-                            {actors}
+                            <b>В главных ролях <img src="checkMark.svg" alt="" /></b>
+                            { actors }
                         </div>
                     </div>
                 </div>
             </div>
+
+            { /* Описание фильма */ }
             <div className="description">
                 <p><b>Описание</b></p>
                 <p>{ props.movie.plot }</p>
