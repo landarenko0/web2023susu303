b6f889b5723ebba0b87264096c0ce94a21ca25fa
Nikita <91719405+landarenko0@users.noreply.github.com>
2023-12-13 18:28:36 +0500

    Added comments

--- React/src/components/movie.js
+++ React/src/components/movie.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+// Плашка фильма, которая находится в списке всех фильмов
 function Movie(props) {
     return (
-        <div className="filmBlock" onClick={() => {props.onClick(props.id)}} >
-            <div className="movieName">{props.movie.title}</div>
+        <div className="filmBlock" onClick={ () => { props.onClick(props.id) } } >
+            { /* Название фильма */ }
+            <div className="movieName">{ props.movie.title }</div>
+
+            { /* Краткая информация о фильме */ }
             <div className="movieBriefInfo">
-                <div className="movieBriefInfoItem">{props.movie.year}</div>
+                <div className="movieBriefInfoItem">{ props.movie.year }</div>
                 <div className="movieBriefInfoItem"><label>|</label></div>
-                <div className="movieBriefInfoItem"><label>{props.movie.genres.join(", ")}</label></div>
+                <div className="movieBriefInfoItem"><label>{ props.movie.genres.join(", ") }</label></div>
             </div>
         </div>
     )
