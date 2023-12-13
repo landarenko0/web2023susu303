1b1eec3bee2cadb15189a706de3332720f4c6ecb
Nikita <91719405+landarenko0@users.noreply.github.com>
2023-12-13 18:29:28 +0500

    Added comment

--- React/src/components/movieBlock.js
+++ React/src/components/movieBlock.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Movie from "./movie";
 
+// Весь блок с плашками фильмов
 function MovieBlock(props) {
-    return props.movies.map((movie, index) => <Movie key={index} id={index} movie={movie} onClick={props.onClick} />)
+    return props.movies.map((movie, index) => <Movie key={ index } id={ index } movie={ movie } onClick={ props.onClick } />)
 }
 
 export default MovieBlock;
