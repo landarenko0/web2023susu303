aafb9022f62509c2bc4fb1e4762425d757122de8
Nikita <91719405+landarenko0@users.noreply.github.com>
2023-12-13 18:27:56 +0500

    Added comments, improved readability

--- React/src/components/editMovie.js
+++ React/src/components/editMovie.js
@@ -1,6 +1,9 @@
 import React from "react";
 
+// Фрагмент редактирования фильма
 function EditMovie(props) {
+
+    // Получение новых параметров фильма из текстовых полей
     function getParameters() {
         return {
             id: props.movie.id,
@@ -18,59 +21,72 @@ function EditMovie(props) {
     return (
         <div>
             <div className="createOrEdit">
-            <p className="mainLable">Редактирование фильма</p>
-            <div>
-                <div className="item">
-                    <p className="itemStyle">Название фильма</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите название фильма" id="movieTitle" defaultValue={ props.movie.title }></input>
+                <p className="mainLable">Редактирование фильма</p>
+
+                { /* Ввод параметров фильма */ }
+                <div>
+                    <div className="item">
+                        <p className="itemStyle">Название фильма</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите название фильма" id="movieTitle" defaultValue={ props.movie.title } />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Жанры</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите жанры фильма (через ;)" id="movieGenres" defaultValue={ props.movie.genres.join(", ") }></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Жанры</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите жанры фильма (через ', ')" id="movieGenres" defaultValue={ props.movie.genres.join(", ") } />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Год выпуска</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите год выпуска" id="movieYear" defaultValue={ props.movie.year }></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Год выпуска</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите год выпуска" id="movieYear" defaultValue={ props.movie.year } />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Описание</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите описание" id="moviePlot" defaultValue={ props.movie.plot }></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Описание</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите описание" id="moviePlot" defaultValue={ props.movie.plot } />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Ссылка на обложку</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Укажите ссылку на обложку" id="moviePosterUrl" defaultValue={ props.movie.posterUrl }></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Ссылка на обложку</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Укажите ссылку на обложку" id="moviePosterUrl" defaultValue={ props.movie.posterUrl } />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Длительность фильма (мин)</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Укажите длительность фильма в минутах" id="movieRuntime" defaultValue={ props.movie.runtime }></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Длительность фильма (мин)</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Укажите длительность фильма в минутах" id="movieRuntime" defaultValue={ props.movie.runtime } />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Список актеров</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите актеров (через ;)" id="movieActors" defaultValue={ props.movie.actors }></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Список актеров</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите актеров (через ', ')" id="movieActors" defaultValue={ props.movie.actors } />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Режиссер</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите режиссера" id="movieDirector" defaultValue={ props.movie.director }></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Режиссер</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите режиссера" id="movieDirector" defaultValue={ props.movie.director } />
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-            <div className="footerLine"></div>
+
+            { /* Нижняя линия */ }
+            <div className="footerLine" />
+            
+            { /* Кнопки отмены и подтверждения */ }
             <div className="createOrEditFooter">
                 <button className="cancelButton" onClick={ () => { props.onCancelClick() } }>Отменить</button>
                 <button className="saveButton" onClick={ () => { props.onSubmit(getParameters()) } }>Сохранить</button>
