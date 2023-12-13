abfd55ad02ec530b602bfb34b5bdedf10e532c9c
Nikita <91719405+landarenko0@users.noreply.github.com>
2023-12-13 18:26:36 +0500

    Added comments, improved readability

--- React/src/components/createMovie.js
+++ React/src/components/createMovie.js
@@ -1,6 +1,9 @@
 import React from "react";
 
+// Фрагмент создания фильма
 function CreateMovie(props) {
+    
+    // Получение параметров фильма из текстовых полей
     function getParameters() {
         return {
             title: document.getElementById("movieTitle").value,
@@ -17,59 +20,72 @@ function CreateMovie(props) {
     return (
         <div>
             <div className="createOrEdit">
-            <p className="mainLable">Создание фильма</p>
-            <div>
-                <div className="item">
-                    <p className="itemStyle">Название фильма</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите название фильма" id="movieTitle"></input>
+                <p className="mainLable">Создание фильма</p>
+
+                { /* Ввод параметров фильма */ }
+                <div>
+                    <div className="item">
+                        <p className="itemStyle">Название фильма</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите название фильма" id="movieTitle" />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Жанры</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите жанры фильма (через ', ')" id="movieGenres"></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Жанры</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите жанры фильма (через ', ')" id="movieGenres" />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Год выпуска</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите год выпуска" id="movieYear"></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Год выпуска</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите год выпуска" id="movieYear" />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Описание</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите описание" id="moviePlot"></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Описание</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите описание" id="moviePlot" />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Ссылка на обложку</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Укажите ссылку на обложку" id="moviePosterUrl"></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Ссылка на обложку</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Укажите ссылку на обложку" id="moviePosterUrl" />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Длительность фильма (мин)</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Укажите длительность фильма в минутах" id="movieRuntime"></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Длительность фильма (мин)</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Укажите длительность фильма в минутах" id="movieRuntime" />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Список актеров</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите актеров (через ', ')" id="movieActors"></input>
+
+                    <div className="item">
+                        <p className="itemStyle">Список актеров</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите актеров (через ', ')" id="movieActors" />
+                        </div>
                     </div>
-                </div>
-                <div className="item">
-                    <p className="itemStyle">Режиссер</p>
-                    <div className="inputStyle">
-                        <input type="text" className="textStyle" placeholder="Введите режиссера" id="movieDirector"></input>
+                    
+                    <div className="item">
+                        <p className="itemStyle">Режиссер</p>
+                        <div className="inputStyle">
+                            <input type="text" className="textStyle" placeholder="Введите режиссера" id="movieDirector" />
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-            <div className="footerLine"></div>
+            
+            { /* Нижняя линия */ }
+            <div className="footerLine" />
+            
+            { /* Кнопки отмены и подтверждения */ }
             <div className="createOrEditFooter">
                 <button className="cancelButton" onClick={ () => { props.onCancelClick() } }>Отменить</button>
                 <button className="saveButton" onClick={ () => { props.onSubmit(getParameters()) } }>Сохранить</button>
